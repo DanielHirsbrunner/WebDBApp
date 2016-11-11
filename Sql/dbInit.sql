8d715edc868413403976993071f2b4642e560362
DanielHirsbrunner <dhirsbrunner@gmail.com>
2016-11-11 10:16:50 +0800

    Database initial checkin

---
+++ Sql/dbInit.sql
@@ -0,0 +1,197 @@
+/**
+ * Web Database Applications - Assignment 2
+ * DB Initial Script for the syllabus management portal
+ * 
+ * Author Daniel Hirsbrunner
+ * Date 2016.11.09
+ */
+DROP DATABASE IF EXISTS moduleinfo;
+CREATE DATABASE moduleinfo;
+
+USE moduleinfo;
+
+CREATE TABLE user (
+  userId int(5) NOT NULL AUTO_INCREMENT,
+  userName varchar(50) NOT NULL UNIQUE,
+  password varchar(50) NOT NULL,
+  name varchar(50),
+  surname varchar(50),
+  email varchar(50),
+  qualification varchar(255),
+  PRIMARY KEY (userId),
+  KEY password (password)
+);
+
+CREATE TABLE module (
+  moduleId int(5) NOT NULL AUTO_INCREMENT,
+  name varchar(50) NOT NULL,
+  code varchar(50) NOT NULL,
+  credits int(2),
+  moduleOwner int(5),
+  purpose text NOT NULL,
+  editBy int(5) NOT NULL,
+  editTS timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  CONSTRAINT fk_module_editBy FOREIGN KEY (editBy) REFERENCES user(userId),
+  CONSTRAINT fk_module_owner FOREIGN KEY (moduleOwner) REFERENCES user(userId),
+  PRIMARY KEY (moduleId)
+);
+
+CREATE TABLE modulePrerequisite (
+  modulePrerequisiteId int(5) NOT NULL AUTO_INCREMENT,
+  moduleId int(5) NOT NULL,
+  moduleIdPrerequisite int(5) NOT NULL,
+  CONSTRAINT fk_module_module FOREIGN KEY (moduleId) REFERENCES module(moduleId),
+  CONSTRAINT fk_module_moduleIdPrerequisite FOREIGN KEY (moduleIdPrerequisite) REFERENCES module(moduleId),
+  PRIMARY KEY (modulePrerequisiteId)
+);
+
+CREATE TABLE moduleRight (
+  moduleRightId int(5) NOT NULL AUTO_INCREMENT,
+  moduleId int(5) NOT NULL,
+  userId int(5) NOT NULL,
+  canRead bit NOT NULL DEFAULT 0,
+  canWrite bit NOT NULL DEFAULT 0,
+  canApprove bit NOT NULL DEFAULT 0,
+  CONSTRAINT fk_moduleRight_user FOREIGN KEY (userId) REFERENCES user(userId) ON DELETE CASCADE,
+  CONSTRAINT fk_moduleRight_module FOREIGN KEY (moduleId) REFERENCES module(moduleId) ON DELETE CASCADE,
+  PRIMARY KEY (moduleRightId)
+);
+
+CREATE TABLE syllabus (
+  syllabusId int(5) NOT NULL AUTO_INCREMENT,
+  moduleId int(5) NOT NULL,
+  versionNr int(5) NOT NULL, 
+  revisionNr int(5) NOT NULL, 
+  academicStuff int(5) NOT NULL,
+  semester varchar(50) NOT NULL,
+  guidedLearnLecture decimal(5,2), 
+  guidedLearnTutorial decimal(5,2), 
+  guidedLearnPractical decimal(5,2), 
+  guidedLearnOther decimal(5,2), 
+  indepLearnLecture decimal(5,2), 
+  indepLearnTutorial decimal(5,2), 
+  indepLearnPractical decimal(5,2),
+  indepLearnOther decimal(5,2), 
+  learningOutcomes text(4000),
+  transferableSkills text(4000),
+  synopsis text(4000),
+  assessmentCode varchar(50) NOT NULL,
+  mainReferences text(4000),
+  addReferences text(4000),
+  addInformation text(4000),
+  approvedBy int(5),
+  approvedTS timestamp,  
+  editBy int(5) NOT NULL,
+  editTS timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  
+  CONSTRAINT fk_syllabus_module FOREIGN KEY (moduleId) REFERENCES module(moduleId) ON DELETE CASCADE,
+  CONSTRAINT fk_syllabus_academicStuff FOREIGN KEY (academicStuff) REFERENCES user(userId),
+  CONSTRAINT fk_syllabus_approvedBy FOREIGN KEY (approvedBy) REFERENCES user(userId),
+  CONSTRAINT fk_syllabus_editBy FOREIGN KEY (editBy) REFERENCES user(userId),
+  PRIMARY KEY (syllabusId)
+);
+
+CREATE TABLE syllabusTopic (
+  syllabusTopicId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  topicNr int(5) NOT NULL,
+  description varchar(255) NOT NULL,
+  
+  guidedLearnLecture decimal(5,2), 
+  guidedLearnTutorial decimal(5,2), 
+  guidedLearnPractical decimal(5,2), 
+  guidedLearnOther decimal(5,2), 
+  indepLearnLecture decimal(5,2), 
+  indepLearnTutorial decimal(5,2), 
+  indepLearnPractical decimal(5,2),
+  indepLearnOther decimal(5,2), 
+  
+  CONSTRAINT fk_syllabusTopic_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  PRIMARY KEY (syllabusTopicId)
+);
+
+
+CREATE TABLE assessmentType (
+  assessmentTypeId int(5) NOT NULL AUTO_INCREMENT,
+  description varchar(255) NOT NULL,
+  PRIMARY KEY (assessmentTypeId)
+);
+Insert into assessmentType(description) values ('Final Exam'), ('Assignment'), ('Assignment/ Documentation'), ('Presentation');
+
+CREATE TABLE syllabusAssessmentType (
+  syllabusAssessmentTypeId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  assessmentTypeId int(5) NOT NULL,
+  CONSTRAINT fk_syllabusAssessmentType_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  CONSTRAINT fk_syllabusAssessmentType_assessmentType FOREIGN KEY (assessmentTypeId) REFERENCES assessmentType(assessmentTypeId),
+  PRIMARY KEY (syllabusAssessmentTypeId)
+);
+
+
+CREATE TABLE mqfSkill (
+  mqfSkillId int(5) NOT NULL AUTO_INCREMENT,
+  description varchar(255) NOT NULL,
+  PRIMARY KEY (mqfSkillId)
+);
+Insert into mqfSkill(description) values ('Knowledge'), ('Practical Skill'), ('Communication'), ('Leadership');
+
+CREATE TABLE syllabusMqfSkill (
+  syllabusMqfSkillId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  mqfSkillId int(5) NOT NULL,
+  CONSTRAINT fk_syllabusMqfSkill_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  CONSTRAINT fk_syllabusMqfSkill_mqfSkill FOREIGN KEY (mqfSkillId) REFERENCES mqfSkill(mqfSkillId),
+  PRIMARY KEY (syllabusMqfSkillId)
+);
+
+
+CREATE TABLE teachLearnActivity (
+  teachLearnActivityId int(5) NOT NULL AUTO_INCREMENT,
+  description varchar(255) NOT NULL,
+  PRIMARY KEY (teachLearnActivityId)
+);
+Insert into teachLearnActivity(description) values ('Lecture'), ('Tutorial'), ('Lab'), ('Group work and Discussion');
+
+CREATE TABLE syllabusTeachLearnActivity (
+  syllabusTeachLearnActivityId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  teachLearnActivityId int(5) NOT NULL,
+  CONSTRAINT fk_syllabusTeachLearnActivity_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  CONSTRAINT fk_syllabusTeachLearnActivity_teachLearnActivity FOREIGN KEY (teachLearnActivityId) REFERENCES teachLearnActivity(teachLearnActivityId),
+  PRIMARY KEY (syllabusTeachLearnActivityId)
+);
+
+
+CREATE TABLE modeOfDelivery (
+  modeOfDeliveryId int(5) NOT NULL AUTO_INCREMENT,
+  description varchar(255) NOT NULL,
+  PRIMARY KEY (modeOfDeliveryId)
+);
+Insert into modeOfDelivery(description) values ('Lecture'), ('Tutorial'), ('Practical'), ('Workshop'), ('Seminar');
+
+CREATE TABLE syllabusModeOfDelivery (
+  syllabusModeOfDeliveryId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  modeOfDeliveryId int(5) NOT NULL,
+  CONSTRAINT fk_syllabusModeOfDelivery_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  CONSTRAINT fk_syllabusModeOfDelivery_modeOfDelivery FOREIGN KEY (modeOfDeliveryId) REFERENCES modeOfDelivery(modeOfDeliveryId),
+  PRIMARY KEY (syllabusModeOfDeliveryId)
+);
+
+CREATE TABLE syllabusProgAim (
+  syllabusProgAimId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  clo int(1) NOT NULL,
+  peo int(1) NOT NULL,
+  CONSTRAINT fk_syllabusProgAim_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  PRIMARY KEY (syllabusProgAimId)
+) COMMENT ='Mapping of the Module to the Programme Aims';
+
+CREATE TABLE syllabusProgLearnOutcome (
+  syllabusProgLearnOutcomeId int(5) NOT NULL AUTO_INCREMENT,
+  syllabusId int(5) NOT NULL,
+  mlo int(1) NOT NULL,
+  plo int(1) NOT NULL,
+  CONSTRAINT fk_syllabusProgLearnOutcome_syllabus FOREIGN KEY (syllabusId) REFERENCES syllabus(syllabusId),
+  PRIMARY KEY (syllabusProgLearnOutcomeId)
+) COMMENT='Mapping of the Module to the Programme Learning Outcomes';
---
+++ Sql/demoData.sql
@@ -0,0 +1,11 @@
+
+Insert into user (userName, password, surname, name, qualification) 
+	values
+		('admin', 'admin', 'admin', 'Administrator', 'Superuser'),
+		('LimEngLye', 'secret', 'Lim Eng', 'Lye', 'Bachelor of Science (Computer Science), Master of Computer Science');
+
+Insert into module (code, name, purpose, credits, editBy) 
+	values 
+		('ITS62304', 'WEB DATABASE APPLICATIONS', 'The purpose of the module is to introduce the use of PHP to generate web pages and to introduce the concepts and skills to develop a database backed, dynamic and feature rich website.', 4, (SELECT userId from user where userName like 'admin' LIMIT 1)),
+		('ITS61104', 'Web Systems and Technologies', 'This module introduces the student to the basics of web technology concepts, the principles and tools that can be used to develop web applications. Topics would include internet protocols, HTML and XML files, client processing with Javascript and server side processing with PHP.', 4, (SELECT userId from user where userName like 'admin' LIMIT 1));
+	
