7800f6d1a0e76993ef0132bbdb66c14b2ab1cd3d
Milan Košťák <milankostak@seznam.cz>
2016-11-17 11:45:44 +0800

    old dbConnect deleted

--- app/dbConnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$dsn = "mysql://moduleinfo:moduleinfo@127.0.0.1/moduleinfo";
-
-$db = DB::connect($dsn);
-if (DB::isError($db)) {
-	var_dump($db);
-	die("Connecting failed: ".$db->getMessage());
-}
